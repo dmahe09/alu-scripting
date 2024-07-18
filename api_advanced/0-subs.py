a4c488069cd7949c1b3ddaf7db449157a78c7234
dmahe09 <m.digne@alustudent.com>
2024-07-18 01:40:20 -0700

    RECORRECT OF 0-subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and
-returns the number of subscribers
-(not active users, total subscribers)
-for a given subreddit.
-If an invalid subreddit is given,
-the function should return 0.
-"""
+'''
+Defines function that queries the Reddit API and returns the
+number of subscribers
+'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a given subreddit.
-    """
-    # Set the Default URL strings
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
+    '''Queries the Reddit API and returns the number of subscribers.
 
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
+    Args:
+        subreddit (str): The name of the subreddit.
 
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=headers,
-                       allow_redirects=False)
+    Returns:
+        int: The number of subscribers. Returns 0 for an invalid subreddit.
+    '''
+    # Check if the subreddit is None or not a string
+    if subreddit is None or not isinstance(subreddit, str):
+        return(0)
 
-    # Checks if the subreddit is invalid
-    if res.status_code in repr([302, 404]):
-        return 0
+    # Base URL for the Reddit API
+    endpoint = 'https://www.reddit.com'
 
-    # Returns the total subscribers of the subreddit
-    return res.json().get('data').get('subscribers')
+    # Custom User-Agent to avoid potential issues
+    headers = {'user-agent': 'Mozilla/5.0 \
+(Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+
+    # Make a GET request to the subreddit's about.json endpoint
+    # allow_redirects=False to prevent automatic redirection
+    info = requests.get('{}/r/{}/about.json'.format(
+            endpoint,
+            subreddit), headers=headers, allow_redirects=False)
+
+    # Check if the request was successful (status code 200)
+    if info.status_code == 200:
+        # Parse the JSON response
+        json_info = info.json()
+        # Extract and return the number of subscribers from the response
+        return(json_info.get('data').get('subscribers'))
+    else:
+        # Return 0 for invalid subreddit or if there's an
+        # issue with the request
+        return(0)
