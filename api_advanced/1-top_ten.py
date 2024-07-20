06792a8c4c721a09b0eb3f0532be74078608067d
dmahe09 <m.digne@alustudent.com>
2024-07-20 08:01:16 -0700

    Edited top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,30 @@
-#!/usr/bin/python3
-"""DOCS"""
+
 import requests
 
-
-def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
+def top_ten(subreddit
+    reddit_url = "https/www.reddit.com/r/{}/hot.json" \
+        .format(subreddit
     headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
-
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
-    else:
+        data = 
+        import requests
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json()
+    posts = data.get('data', {}).get('children', [])
+
+    if not posts:
+        print(None)
+        return
+
+    for post in posts[:10]:
+        print(post.get('data', {}).get('title'))
